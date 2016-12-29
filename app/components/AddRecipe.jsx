056dd7fcc5eb9125d8e4b21cc72dd27201943fc3
George <loaizageorge@gmail.com>
2016-12-29 11:59:31 -0500

    Add non functional modal to add recipe

--- app/components/AddRecipe.jsx
+++ app/components/AddRecipe.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import RecipeAPI from 'RecipeAPI';
+import MyModal from 'Modal';
 
 class AddRecipe extends React.Component{
   constructor(props){
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.openModal = this.openModal.bind(this);
   }
   handleSubmit (e) {
     e.preventDefault();
@@ -16,10 +18,15 @@ class AddRecipe extends React.Component{
     this.refs.ingredient.value = '';
     this.props.onSubmit(recipe);
   }
+  openModal(){
+    return(
+      <Modal/>
+    );
+  }
   render(){
     return(
       <div>
-            <form className = "add-form" onSubmit = {this.handleSubmit} >
+            { /*<form className = "add-form" onSubmit = {this.handleSubmit} >
               <div className = "form-group">
                 <label className = "sr-only" for = "form-name">Recipe</label>
                 <input id = "form-name" className = "add-recipe-input"  type = "text" ref = "name" placeholder = "Recipe name: e.g. Apple Pie"/>
@@ -28,16 +35,17 @@ class AddRecipe extends React.Component{
                 <label className = "sr-only" for = "form-ingredients">Ingredients</label>
                   <textarea id = "form-ingredients" className = "add-recipe-input" rows = "5" ref = "ingredient" placeholder ="Ingredients seperated by a comma: e.g. eggs,milk,flour"/>
               </div>
-              {/*<div>
+              <div>
                 Image URL<input type = "text" name = "img" placeholder = "eg. www.google.com/applepiepics"/>
               </div>
               <div>
                 Ingredients<input type = "text" name = "ingredients" placeholder ="Enter ingredients seperated by a comma, for example: apple,pie"/>
-              </div> */}
+              </div>
               <div className = "form-group">
               <input className = "btn btn-primary" type = "submit" value = "Add Recipe"/>
               </div>
-            </form>
+            </form>*/}
+            <MyModal/>
       </div>
     );
   }
---
+++ app/components/Modal.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Modal} from  'react-bootstrap';
+import {Button} from  'react-bootstrap';
+
+class MyModal extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.close = this.close.bind(this);
+    this.open = this.open.bind(this);
+  }
+  close() {
+    this.setState({ showModal: false });
+  }
+  open() {
+    this.setState({ showModal: true });
+  }
+  render() {
+    return (
+      <div>
+        <button type = "button" className = "btn btn-primary" onClick = {this.open}> Add recipe </button>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Text in a modal</h4>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+            <hr />
+
+            <h4>Overflowing text to show scroll behavior</h4>
+            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+};
+
+export default MyModal;
